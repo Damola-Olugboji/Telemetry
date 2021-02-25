92325716e39a79102d2e6264f15300a747869e5e
Damola-Olugboji <damolaolugboji@gmail.com>
2021-02-24 22:43:11 -0500

    simple ssh complete

--- Ground_Computer/main.py
+++ Ground_Computer/main.py
@@ -1,53 +1,68 @@
-from tkinter import *
+import tkinter as tk
 import paramiko
 import math, sys, os
 import config
 
-root = Tk()
-root.title("Flight Computer")
-root.geometry("300x300")
-root.configure(bg="#233342")
 
-
-def initialize_connection():
-    connectionButton.configure(state="disabled")
-    host = "192.168.1.11"
-    port = 22
-    username = "pi"
+class GroundComputer(tk.Frame):
+    host = config.HOST
+    port = config.PORT
+    username = config.USERNAME
     password = config.PASSWORD
 
-    command = "cd Desktop/code/"
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, port, username, password)
-    except SSHExepction:
-        statusLabel.configure(text="Error", fg="red", bg="#233342")
+    @classmethod
+    def main(cls):
+        tk.NoDefaultRoot()
+        root = tk.Tk()
+        root.title("Ground Computer")
+        root.geometry("1100x630+30+30")
+        root.configure(bg="#233342")
+        cls(root).grid(sticky="nsew")
+        root.minsize(1100, 680)
+        root.maxsize(1100, 680)
+        root.mainloop()
 
-    stdin, stdout, stderr = ssh.exec_command(command)
-    flight_control_screen()
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+        self.connectionButton = StatefulButton(
+            self,
+            "Initialize Connection",
+            self.initialize_connection,
+        )
+        self.statusLabel = tk.Label(self, text="", fg="red", bg="#233342")
+
+        self.grid_widgets(padx=10, pady=10)
+
+    def grid_widgets(self, **kw):
+        self.connectionButton.grid(row=0, column=0, sticky="nsew", **kw)
+        self.statusLabel.grid(row=0, column=1, **kw)
+
+    def initialize_connection(self):
+        self.connectionButton.configure(state="disabled")
+        # command = "cd Desktop/code/python3 main.py"
+        command = "ls"
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(self.host, self.port, self.username, self.password)
+        except SSHExepction:
+            statusLabel.configure(text="Error", fg="red", bg="#233342")
+
+        stdin, stdout, stderr = ssh.exec_command(command)
+        print("success")
+        self.statusLabel.configure(text="Success", fg="green", bg="#233342")
 
 
-connectionButton = Button(
-    root,
-    text="Initialize Connection",
-    fg="white",
-    bg="#B35340",
-    command=initialize_connection,
-)
-connectionButton.pack(pady=100)
-statusLabel = Label(text="", bg="#233342")
-statusLabel.pack(pady=25)
+class StatefulButton(tk.Button):
+    def __init__(self, master, text, command, **kw):
+        kw.update(text=text, command=self.__execute_command)
+        super().__init__(master, **kw)
+        self.configure(bg="#233342", fg="white")
+        self.__command = command
+
+    def __execute_command(self):
+        self.__command = self.__command()
 
 
-class flight_control_screen:
-    def __init__(self):
-        top = Toplevel()
-        # app.geometry("1100x630+30+30")
-        top.minsize(1100, 680)
-        top.maxsize(1100, 680)
-    
-    def 
-
-
-root.mainloop()
+if __name__ == "__main__":
+    GroundComputer.main()
